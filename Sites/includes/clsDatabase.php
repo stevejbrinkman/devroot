6c798588b16b8bc378657940db03178f560fd15a
stevejbrinkman <58307059+stevejbrinkman@users.noreply.github.com>
2019-12-08 07:40:30 -0700

    continuous development

--- Sites/includes/clsDatabase.php
+++ Sites/includes/clsDatabase.php
@@ -1,17 +1,12 @@
 <?php
-//This file contains classes to operate on the database.
-
-class clsDemo{
-  public $demoString = "myDemoString";
-}
 
 class clsSampleQuery{
-
+  
   public function demoQuery(){
     // PHP constant DIRECTORY_SEPARATOR will resolve correct form of / or \
     // depending on whether unix or windows web server.
     //if (include '.' . DIRECTORY_SEPARATOR . 'config.php'){
-    if (include 'config.php'){
+    if (require 'config.php'){
     $con = new mysqli($dbSettings['host'], $dbSettings['username'], $dbSettings['pwd'], $dbSettings['dbName']);
     $message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
     $con->close();
@@ -22,16 +17,72 @@ class clsSampleQuery{
   public $demoString2 = "mydbClassString";
 }
 
+class sakila{
+  public function connect(){
+    mysqli_report(MYSQLI_REPORT_STRICT);
+      if (require 'config.php'){
+        try{
+          return new mysqli($dbSettings['host'], $dbSettings['WebUserName'], $dbSettings['WebUserPwd'], $dbSettings['sakilaDbName']);
+          
+        }
+        catch (Exception $e) {
+          echo $e->getMessage();
+          return;
+        }
+    }
+
+  }
+
+  /** Use stored proc to get films and actors join */
+  public function actorsFilms($pTitle){
+      $con = $this->connect();
+      if (!$con){return;}
+
+      $sql = "CALL Actors_Films('".$pTitle."')";
+      $query = $con->multi_query($sql);
+      if ($result = $con->store_result()){
+        $con->close();
+        return $result;
+      }
+      else{
+        echo "no results in Actors_Films";
+        $con->close();
+        return;
+      }
+
+  }
+  /** Get all movie titles */
+  public function getMovies($title=""){
+    $con = $this->connect();
+      if (!$con){return;}
+
+      $sql = "SELECT title FROM film".$where;
+      if ($con->multi_query($sql)){
+        if ($result = $con->store_result()){
+          $con->close();
+          return $result;
+        }
+        else{
+          echo "no results in movies";
+          $con->close();
+          return;
+        }
+      } else echo $con->error;
+    }
+}
+
 class Sales{
 
   public function getAllProductNames(){
-    if (include 'config.php'){
+    if (require 'config.php'){
       $con = new mysqli($dbSettings['host'], $dbSettings['WebUserName'], $dbSettings['WebUserPwd'], $dbSettings['dbName']);
       $sql = "CALL p_productsList()";
       $query = $con->multi_query($sql);
       while ($con->more_results()){
           if ($result = $con->store_result()) {
               while ($row = mysqli_fetch_assoc($result)) {
+                /*foreach(array_keys($row) as $paramName)
+                        echo $paramName . "<br>";*/
                   echo "row = ".$row["ProdName"]."<br />";
                 }
                 mysqli_free_result($result);
--- Sites/includes/clsGrid.php
+++ Sites/includes/clsGrid.php
@@ -1,23 +1,41 @@
 <?php
-include "config.php";
+require "config.php";
+
+require_once "clsDatabase.php";
 class dbGrid{
     public $con;
     public $headerBorderWidth;
     public $cellBorderWidth;
     public $colWidth = array();
     public $rowHeight = array();
-
-
-    public function connect($host, $usr, $pwd, $db){
-        $con = new mysqli($dbSettings['host'], $dbSettings['username'], $dbSettings['pwd'], $dbSettings['dbName']);
-    }
-    public function doGrid(){
+    public $sql;
+    public $filter;
+    
+    /*public $dataSourceType = dsType::Sql;*/
+    
+    public function doGrid($pTitle){
+        
         echo "<div class='grid-container'>";
-        echo "<div class='grid-item'>1</div>";
-        echo "<div class='grid-item'>22</div>";
-        echo "<div class='grid-item'>333</div>";
-        echo "<div/>";
+        $hdrBgStyle = "style='background-color: rgba(184, 201, 92, 0.596)";
+        $rowCount = 0;
+        $maxRows = 25;
+        $sakila = new sakila(); 
+        if ($results = $sakila->actorsFilms($pTitle)){
+            echo "<div class='grid-item'".$hdrBgStyle.";'><h2>"."Movie title"."<h2/></div>";
+            echo "<div class='grid-item'".$hdrBgStyle.";'><h2>"." Star first name"."<h2/></div>";
+            echo "<div class='grid-col-hdr-item'".$hdrBgStyle.";'><h2>"."last name"."<h2/></div>";
+        
+            while  (($rowCount < $maxRows) && ($row = mysqli_fetch_assoc($results))) {
+                
+                    echo "<div>".$row["title"]."</div>";
+                    echo "<div>".$row["first_name"]."</div>";
+                    echo "<div>".$row["last_name"]."</div>";
+                    $rowCount++;
+            
+          }
+        }
+        
+       
     }
             
-
 }
